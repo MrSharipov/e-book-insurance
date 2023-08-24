671037c81241022e38cd34d83afcdabe93033f1b
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2023-08-24 22:02:44 +0500

    minor changes

--- src/components/page/5-topic.js
+++ src/components/page/5-topic.js
@@ -5,16 +5,144 @@ import mainImg from "../../files/insurance.webp";
 export const TopicFive = () => {
   return (
     <div className="page">
-      <h3>Lorem ipsum dolor sit amet.</h3>
-      <h4>Lorem ipsum dolor sit amet.</h4>
+      <h3>
+        V BOB. SUG‘URTA BIZNESIDA SUG‘URTA VOSITACHILARNING TUTGAN O‘RNI VA ULAR
+        BILAN O‘ZARO MUNOSABATLARNI TASHKIL ETIlISHI
+      </h3>
+      <h4>5.1 Sug‘urta biznesida sug‘urta vositachilarning tutgan o‘rni</h4>
       <img src={mainImg} className="mainTopic" alt="insurance" />
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
-        perspiciatis ea excepturi dolorem animi saepe qui quaerat sequi,
-        necessitatibus ex hic ipsum, quis aut nulla distinctio exercitationem
-        ut? Explicabo exercitationem nam dolores. Quidem dolorem molestias
-        expedita ab a, doloremque nam dolor ipsa reprehenderit laborum magni
-        cumque ut temporibus provident voluptate.
+        O’zbekistonning milliy manfaatlariga mos keladigan qator vazifalarni
+        muvaffaqiyatli amalga oshirishda iqtisodiy faoliyatning o‘zaro uyg‘un
+        dastaklari majmui - bozor institutlarini, jumladan, bank va sug‘urta
+        sohalarini rivojlantirish va takomillashtirishga ustuvorlik berish muhim
+        omillardan biri hisoblanadi. O’zbekistonda, sug‘urta xizmatlari
+        bozorining yanada rivojlanishi uchun qulay shart-sharoitlarni yaratish
+        bo‘yicha amalga oshirilayotgan tadbirlar natijasida sug‘urta
+        operatsiyalarining hajmi barqaror o‘sib bormoqda va
+        sug‘urtalovchilarning mamlakat iqtisodiyotdagi o‘rni tobora katta
+        ahamiyat kasb etib bormoqda, shuningdek, jismoniy va yuridik
+        shaxslarning sug‘urta mahsulotlariga bo‘lgan manfaatdorligini oshirishga
+        xizmat qilmoqda.
+      </p>
+      <p>
+        Sug’urta vositachilari sug’urta xizmatlarini keng miqyosda ommaga
+        yetkazish uchun muhim rol o’ynaydi. Sug’urta vositachilari faoliyatining
+        muhim jihatlari sug’urta xizmatlari bozorini harakat qildirish hamda
+        sug’urta xizmatlarini qabul qiluvchisiga maslahat va yordamini
+        ko’rsatishdan iborat. Darhaqiqat, O’zbekiston Respublikasining “Sug’urta
+        faoliyati to’g’risida”gi qonunining 5-moddasida sug’urta brokeri, qayta
+        sug’urta brokeri sug’urta agenti sug’urta vositachilari deb yuritilishi
+        aytib o’tilgan.
+      </p>
+      <p>
+        O’zbekiston Respublikasida sug’urta vositachilarining faoliyati quyidagi
+        qonunchilik hujjatlari bilan tartibga solinadi:
+      </p>
+      <ol>
+        <li>
+          O’zbekiston Respublikasining “Sug’urta faoliyati to’g’risida”gi
+          qonuni.
+        </li>
+        <li>
+          O’zbekiston Respublikasi Vazirlar Mahkamasining 2002-yil 27-noyabrdagi
+          “Sug’urta xizmatlari bozorini yanada rivojlantirish chora-tadbirlari
+          to’g’risida” gi 413-sonli qarori.
+        </li>
+        <li>
+          O’zbekiston Respublikasi Moliya Vazirligining 2003-yil 28-yanvardagi
+          19-sonli buyrug’i bilan tasdiqlangan va Adliya vazirligida davlat
+          ro’yxatiga olingan “Sug’urta agentlari to’g’risida”gi Nizom.
+        </li>
+        <li>
+          O’zbekiston Respublikasi Moliya Vazirligi tomonidan 2010-yil
+          10-noyabrda tasdiqlangan “Sug’urtalovchilarning sug’urta vositachilari
+          bilan o’zaro munosabatini tartibga soluvchi namunaviy qoidalar”.
+        </li>
+      </ol>
+      <p>
+        Sug’urta vositachilarining faoliyati sug’urta shartnomalarini tuzish va
+        uning ijrosini ta’minlash bo’yicha faoliyatni tashkil etishdan iborat.
+        Sug‘urta tashkilotlari tomonidan jismoniy va yuridik shaxslarning keng
+        qatlamini turli qaltisliklardan ishonchli himoyasini ta’minlashda
+        sug‘urta vositachilarining o‘rin muhimdir. Bugungi kunda sug‘urta
+        tashkilotlarining ham, ularning mahsulotlari iste’molchilari bo‘lgan
+        jismoniy va yuridik shaxslar ham sug‘urta vositachilarining xizmatlariga
+        ehtiyojlari tobora ortib bormoqda.
+      </p>
+      <p>
+        Sug‘urta vositachilarining asosiy faoliyatlari bu mijozlarga sug‘urta
+        shartnomalari tuzish va ularni ijro etishda yaqindan ko‘mak berishdan
+        iborat. Bundan tashqari sug‘urta vositachilari aholining sug‘urta
+        madaniyatini oshishida, sug‘urta institutlariga, qolaversa, sug‘urta
+        institutlariga bo‘lgan ishonchning vujudga kelishida katta ahamiyatga
+        ega.
+      </p>
+      <p>
+        Hozirgi kunda vositachilar orqali polislar xarid qilish ko‘pchilik
+        mamlakatlarda va aksariyat odamlar uchun eng umumiy tendensiya
+        hisoblanadi. Sug‘urta vositachisining, boshqa sohalardagi, aytaylik,
+        savdo sohasidagi vositachilardan farqi shundan iboratki, u hech qanday
+        zahira qilmaydi va keyinchalik sotish uchun oldindan zahira jamlab
+        qo‘ymaydi . Zamonaviy sug‘urta bozori ko‘p sonli sug‘urtalovchilar va
+        operatsiyalar hajmining ko‘pligi, sug‘urta faoliyatidagi mavjud
+        yo‘nalishlarning rivojlanishi va yangi yo‘nalishlarning vujudga kelishi,
+        raqobatning kuchayishi va milliy bozorga xorijiy sug‘urtalovchilarning
+        kelishi bilan xarakterlanadi. Bunday sharoitlarda sug‘urtalovchilarning
+        sug‘urta mahsulotlarining sotuvini ta’minlashda sug‘urta
+        vositachilarining ahamiyati katta. Sug‘urta bozori infrastrukturasini
+        shakllantirish va sug‘urta xizmatlarini taraqqiy ettirishda ularga
+        alohida xizmat yuklatiladi. Garchi sug‘urta vositachilari sug‘urta
+        shartnomasining ishtirokchilari bo‘lmasalar ham, sug‘urtalovchidan
+        sug‘urta qiluvchiga ko‘rsatiladigan sug‘urta xizmatlari: sug‘urta
+        shartnomasining ijroga muvofiqligi, shartnoma tuzilish bosqichlarida
+        sug‘urta qildiruvchiga maslahat ko‘magi berish, sug‘urta shartnomasining
+        bajarilishi, modifikatsiyasi (shaklining va turining o‘zgartirilishi) va
+        shartnomaning tugatilishi hamda qaytadan sug‘urta shartnomasi tuzishda
+        ishtirok etish ularning vazifasiga kiradi.
+      </p>
+      <p>
+        Modomiki, sug‘urta bozori turli sug‘urta kompaniyalari, ularning
+        sug‘urta mahsulotlari va shartlarining son-sanoqsizligi bilan ifodalanar
+        ekan, sug‘urta vositachilari sug‘urta qildiruvchiga unga mos sug‘urta
+        mahsulotini tanlashda yordam beradi. Vositachi mijozga mavjud xizmatlar
+        to‘g‘risida ma’lumot beradi, tavsiya qiladi va maslahat beradi,
+        sug‘urtalashning kutilayotgan variantlari bilan sug‘urta qildiruvchini
+        ta’minlaydi.
+      </p>
+      <p>
+        Sug‘urtalash va qayta sug‘urtalashda sug‘urta agentlari, sug‘urta
+        brokerlarining faoliyati sug‘urtalovchi va sug‘urta qildiruvchining
+        qiziqishlari asosida amalga oshiriladi va ularga sug‘urta qildiruvchiga
+        sug‘urtalovchi (qayta sug‘urtalovchi) xizmatlarini tanlashda, sug‘urta
+        (qayta sug‘urtalash) shartlari, sug‘urta (qayta sug‘urtalash)
+        shartnomasini rasmiylashtirish, tuzish va qaytadan tuzilgan sug‘urta
+        shartnomasi hamrohligida unga kiritiladigan o‘zgarishlar, sug‘urta
+        to‘lovini talab qilish tartibi va hujjatlarni rasmiylashtirish,
+        sug‘urtalovchi (qayta sug‘urtalovchi) bilan o‘zaro ta’siri, maslahat
+        faoliyatini amalga oshiradi.
+      </p>
+      <p>
+        Sug‘urtalanuvchilarga tez va sifatli xizmat ko‘rsatish yo‘li bilan
+        sug‘urta qoplamasini to‘lab berilishi: bir tarafdan sug‘urta
+        kompaniyasining obro‘si oshishiga va buning orqasidan yangi mijozlarni
+        jalb qilsa ikkinchi tarafdan,- misol uchun katta xalokatlar yuz berganda
+        ishlab chiqarishni tezroq tiklashga ehtiyoj seziladi, bu esa o‘z
+        navbatida makroiqtisodiy barqarorlikni ta’minlashga sabab bo‘ladi.
+        Sug‘urta vositachilari bir tomondan sug‘urta xizmatlarining oxirgi
+        iste’molchilari - sug‘urtalanuvchilar bilan sug‘urta munosobatlariga
+        kirishadilar, ikkinchi tomondan esa ular sug‘urta qoplamalarini to‘lash
+        majburiyatlarini olmaydi va javobgarlik olib bormaydi.
+      </p>
+      <p>
+        Har qanday vositachilar (sug‘urta agenti, sug‘urta brokeri) o‘z
+        faoliyatlarida malakali mutaxassis bo‘lishlari (malaka sertifikatiga)
+        juda muhim o‘rin tutadi. Sababi, mahsulotning sifati bu ishlab
+        chiquvchiga ayon bo‘lgan holatdir, uning sifatli va afzalligini esa
+        iste’molchilarga aynan vositachi yetkazadi. Shuningdek, xorij
+        tajribasida mahsulotni kamdan kam holatda ishlab chiqaruvchining o‘zi
+        taqdim etadi yoki sotadi. Bu ayniqsa oxirgi yillarda elektron tijorat
+        rivojlanishida keng kuzatiladigan odatiy holatga aylanib qoldi .
       </p>
     </div>
   );
--- src/components/page/6-topic.js
+++ src/components/page/6-topic.js
@@ -5,8 +5,11 @@ import mainImg from "../../files/insurance.webp";
 export const TopicSix = () => {
   return (
     <div className="page">
-      <h3>Lorem ipsum dolor sit amet.</h3>
-      <h4>Lorem ipsum dolor sit amet.</h4>
+      <h3>VI BOB. SUG‘URTA BIZNESIDA MOLIYAVIY FAOLIYATNI BYUDJETLASHTIRISH</h3>
+      <h4>
+        6.1 Sug‘urta tashkilotlarida moliyaviy resurslarni joylashtirishning
+        o‘ziga xos xususiyatlari
+      </h4>
       <img src={mainImg} className="mainTopic" alt="insurance" />
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere
